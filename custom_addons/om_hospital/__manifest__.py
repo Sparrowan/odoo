631657b14d8cb96c0d1e85ed9607065aaa086a84
Admin <admin@MacBook-Air.local>
2024-09-12 18:17:31 +0300

    Added custom module

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Hospital Management System",
+    "author": "Alphius Wambua",
+    "license": "LGPL-3",
+    "version": "17.0.1.0",
+    "data": [
+        "views/menu.xml"
+    ]
+}
