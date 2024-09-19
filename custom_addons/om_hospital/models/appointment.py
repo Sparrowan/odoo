93464dc896f6b34a2ec035638e6084e1d19cbea8
Alphius <alphiuswambua@gmail.com>
2024-09-19 18:59:12 +0300

    Added sequences

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -6,6 +6,14 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
     _rec_name='patient_id'
 
+    reference = fields.Char(string="Reference", default='New')
     patient_id = fields.Many2one('hospital.patient', string="Patient", required=True)
     date_appointment = fields.Date(string="Date")
     note = fields.Text(string="Note")
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('reference') or vals['reference'] == 'New':
+                vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
+        return super().create(vals_list)
